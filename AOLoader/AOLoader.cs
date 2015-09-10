bb8554bf358c5bb3bfceb9137b85c916020513ab
Austin Bingham <austin@sixty-north.com>
2015-09-10 09:57:33 +0200

    Added library to load AO Code yaml files

---
+++ AOLoader/AOLoader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using YamlDotNet.RepresentationModel;
+
+namespace AOLoader
+{
+    public class AOLoader
+    {
+        public struct Classification
+        {
+            public string Code;
+            public string[] Name;
+            public string description;
+        }
+
+        private static IEnumerable<Classification> load(YamlMappingNode node)
+        {
+            foreach (var entry in node.Children)
+            {
+                yield return new Classification()
+                {
+                    Code = ((YamlScalarNode)entry.Key).Value
+                };
+            }
+        }
+
+        public static IEnumerable<Classification> load(StreamReader input)
+        {
+            // Load the stream
+            var yaml = new YamlStream();
+            yaml.Load(input);
+
+            var root = (YamlMappingNode)yaml.Documents[0].RootNode;
+            return load(root);
+        }
+
+        public static IEnumerable<Classification> loadFile(string filename)
+        {
+            using (var input = new StreamReader(new FileStream(filename, FileMode.Open)))
+            {
+                return load(input);                
+            }      
+        }
+    }
+}
---
+++ AOLoaderTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AOLoader;
+
+namespace AOLoaderTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var c in AOLoader.AOLoader.loadFile(args[0]))
+            {
+                Console.WriteLine(c.Code);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
